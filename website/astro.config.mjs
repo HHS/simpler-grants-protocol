4dba032d36b9d21b2e129090c6e0613437e6c1b4
widal001 <billy.daly@agile6.com>
2025-01-21 09:45:35 -0500

    ci: Adds starlight-link-validator and fixes broken links

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   base: "simpler-grants-protocol/",
   integrations: [
     starlight({
+      plugins: [starlightLinksValidator()],
       title: "CommonGrants",
       social: {
         github: "https://github.com/HHS/simpler-grants-protocol",
