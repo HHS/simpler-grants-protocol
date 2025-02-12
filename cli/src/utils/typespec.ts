cacef8512e2d177b51594bec8d5aed968229568f
Billy Daly <billy.daly@agile6.com>
2025-02-12 09:58:49 -0500

    [Issue #40] Fix CLI dependencies (#80)
    * feat(templates): Adds back default API template
    * build(cli): Adds @typespec/compiler as a direct dependency
    * refactor(cli): Updates how tsp path is resolved
    * build(cli): Bumps pre-release version

--- cli/src/utils/typespec.ts
+++ cli/src/utils/typespec.ts
@@ -1,3 +1,4 @@
-import { join } from "path";
-
-export const tspBinPath = join(__dirname, "../../node_modules/.bin/tsp");
+// Resolves the path to the tsp binary
+// This allows us to provide a thin wrapper around `tsp` commands
+// like `tsp init` and `tsp compile` that we want to call from the CLI
+export const tspBinPath = require.resolve(".bin/tsp");
