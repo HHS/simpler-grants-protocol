c8771d53e12d5a37b23faa6b0d0b0a195bfea585
jcrichlake <jeff.crichlake@agile6.com>
2025-04-01 11:52:16 -0400

    Updating import

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,7 +2,7 @@
 
 import { program } from "commander";
 import { initCommand } from "./commands/init/init";
-import { previewCommand } from "./commands/preview";
+import { previewCommand } from "./commands/preview/preview";
 import { checkCommand } from "./commands/check/check";
 import { compileCommand } from "./commands/compile/compile";
 
