cc3d427c5e5549ccf4940c95cd274c850a9f1845
widal001 <billy.daly@agile6.com>
2025-05-30 15:29:49 -0400

    feat(cli): Adds new error type and collection These new error handlers will help us improve the `check spec` output

---
+++ cli/src/commands/check/utils/error-utils.ts
@@ -0,0 +1,89 @@
+import { NewComplianceError, ErrorType } from "./types";
+
+// #########################################################
+// ErrorCollection
+// #########################################################
+
+export class ErrorCollection {
+  private errors: NewComplianceError[] = [];
+  private endpoints: Set<string> = new Set();
+
+  /** Add a single error to the ErrorCollection */
+  addError(error: NewComplianceError): void {
+    this.errors.push(error);
+    // Add endpoint to set if it exists
+    if ("endpoint" in error) {
+      this.endpoints.add(error.endpoint);
+    }
+  }
+
+  /** Add multiple errors to the ErrorCollection */
+  addErrors(errors: NewComplianceError[]): void {
+    errors.forEach(error => {
+      this.addError(error);
+    });
+  }
+
+  /** Get a new ErrorCollection filtered by error type */
+  filterByType(type: ErrorType): ErrorCollection {
+    const filtered = new ErrorCollection();
+    filtered.addErrors(this.errors.filter(error => error.type === type));
+    return filtered;
+  }
+
+  /** Get a new ErrorCollection filtered by endpoint */
+  filterByEndpoint(endpoint: string): ErrorCollection {
+    const filtered = new ErrorCollection();
+    filtered.addErrors(this.errors.filter(error => error.endpoint === endpoint));
+    return filtered;
+  }
+
+  /** Get a new ErrorCollection filtered by level */
+  filterByLevel(level: "ERROR" | "WARNING"): ErrorCollection {
+    const filtered = new ErrorCollection();
+    filtered.addErrors(this.errors.filter(error => error.level === level));
+    return filtered;
+  }
+
+  /** Get the total number of errors in the ErrorCollection */
+  getErrorCount(): number {
+    return this.errors.length;
+  }
+
+  /** Get the total number of endpoints in the ErrorCollection */
+  getEndpointCount(): number {
+    return this.endpoints.size;
+  }
+
+  /** Get a list of all errors in the ErrorCollection */
+  getAllErrors(): NewComplianceError[] {
+    return [...this.errors];
+  }
+
+  /** Make ErrorCollection iterable
+   * @returns An iterator over the errors in the ErrorCollection
+   * @example
+   * ```ts
+   * const errors = new ErrorCollection();
+   * errors.addError({ type: "ERROR", level: "ERROR", endpoint: "/api/v1/users" });
+   * errors.addError({ type: "WARNING", level: "WARNING", endpoint: "/api/v1/users" });
+   *
+   * // Using for...of
+   * for (const error of errors) {
+   *   console.log(error);
+   * }
+   *
+   * // Using spread operator
+   * const errorArray = [...errors];
+   *
+   * // Using Array.from
+   * const errorArray2 = Array.from(errors);
+   *
+   * // Using destructuring
+   * const [firstError, ...restErrors] = errors;
+   * ```
+   */
+  [Symbol.iterator](): Iterator<NewComplianceError> {
+    return this.errors[Symbol.iterator]();
+  }
+}
--- cli/src/commands/check/utils/types.ts
+++ cli/src/commands/check/utils/types.ts
@@ -20,3 +20,42 @@ export interface OperationObject extends OpenAPIV3.OperationObject {
 }
 
 export type ResponseObject = OpenAPIV3.ResponseObject;
+
+// #########################################################
+// Error types
+// #########################################################
+
+export type ErrorType = "MISSING_ROUTE" | "EXTRA_ROUTE" | "ROUTE_CONFLICT";
+export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
+
+export interface BaseError {
+  type: ErrorType;
+  level: "ERROR" | "WARNING";
+  endpoint: string;
+  message?: string;
+}
+
+export type ErrorSubType =
+  | "MISSING_STATUS_CODE"
+  | "REQUEST_BODY_CONFLICT"
+  | "RESPONSE_BODY_CONFLICT"
+  | "MISSING_QUERY_PARAM"
+  | "EXTRA_QUERY_PARAM"
+  | "QUERY_PARAM_CONFLICT";
+
+export type SchemaConflictType =
+  | "TYPE_CONFLICT"
+  | "MISSING_FIELD"
+  | "EXTRA_FIELD"
+  | "ENUM_CONFLICT";
+
+export interface SchemaConflictError extends BaseError {
+  type: "ROUTE_CONFLICT";
+  subType: ErrorSubType;
+  schemaPath: string;
+  baseType?: string;
+  implType?: string;
+  conflictType: SchemaConflictType;
+}
+
+export type NewComplianceError = SchemaConflictError | BaseError;
