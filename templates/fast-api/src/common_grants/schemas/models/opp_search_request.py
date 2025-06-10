168a5901d41fa48748eda035b2c048adfa26d756
David Dudas <david.dudas@intuitial.com>
2025-06-09 19:49:45 -0700

    encapsulate search request

---
+++ templates/fast-api/src/common_grants/schemas/models/opp_search_request.py
@@ -0,0 +1,15 @@
+"""Request model for opportunity search."""
+
+from pydantic import BaseModel
+
+from common_grants.schemas.models import OppFilters
+from common_grants.schemas.pagination import PaginationBodyParams
+from common_grants.schemas.sorting import OppSortBy, OppSorting
+
+
+class OpportunitySearchRequest(BaseModel):
+    """Request body for searching opportunities."""
+
+    filters: OppFilters = OppFilters()
+    sorting: OppSorting = OppSorting(sortBy=OppSortBy.LAST_MODIFIED_AT)
+    pagination: PaginationBodyParams = PaginationBodyParams() 
