f952333afc1f4eafa33cb91f7816920ad5674281
David Dudas <david.dudas@intuitial.com>
2025-07-16 11:00:17 -0700

    import not require

--- lib/cli/src/index.ts
+++ lib/cli/src/index.ts
@@ -5,8 +5,8 @@ import { initCommand } from "./commands/init/init";
 import { previewCommand } from "./commands/preview/preview";
 import { checkCommand } from "./commands/check/check";
 import { compileCommand } from "./commands/compile/compile";
+import packageJson from "../package.json";
 
-const packageJson = require("../package.json");
 program.name("cg").description("CommonGrants CLI tools").version(packageJson.version);
 
 // Register commands
