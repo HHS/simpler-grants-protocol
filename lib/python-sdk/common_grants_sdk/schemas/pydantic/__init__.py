511457d119c875b0d36d33037b15ab437733f103
David Dudas <david.dudas@intuitial.com>
2025-08-27 18:02:18 -0700

    simplified exports

--- lib/python-sdk/common_grants_sdk/schemas/pydantic/__init__.py
+++ lib/python-sdk/common_grants_sdk/schemas/pydantic/__init__.py
@@ -1,142 +1,15 @@
 """CommonGrants pydantic schemas package."""
 
-from .base import CommonGrantsBaseModel
-from .fields import (
-    CustomField,
-    CustomFieldType,
-    DateRangeEvent,
-    Event,
-    EventType,
-    Money,
-    OtherEvent,
-    SingleDateEvent,
-    SystemMetadata,
-)
-from .filters import (
-    # Base Filter
-    DefaultFilter,
-    # Operators
-    ArrayOperator,
-    StringOperator,
-    ComparisonOperator,
-    RangeOperator,
-    EquivalenceOperator,
-    # Date Filters
-    DateComparisonFilter,
-    DateRange,
-    DateRangeFilter,
-    # Money Filters
-    MoneyComparisonFilter,
-    MoneyRange,
-    MoneyRangeFilter,
-    InvalidMoneyValueError,
-    # Opportunity Filters
-    OppDefaultFilters,
-    OppFilters,
-    # String Filters
-    StringArrayFilter,
-    StringComparisonFilter,
-)
-from .models import (
-    OpportunityBase,
-    OppFunding,
-    OppStatus,
-    OppStatusOptions,
-    OppTimeline,
-)
-from .pagination import (
-    PaginatedBase,
-    PaginatedBodyParams,
-    PaginatedQueryParams,
-    PaginatedResultsInfo,
-)
-from .requests import OpportunitySearchRequest
-from .responses import (
-    DefaultResponse,
-    Error,
-    Filtered,
-    FilterInfo,
-    OpportunitiesListResponse,
-    OpportunitiesSearchResponse,
-    OpportunityResponse,
-    Paginated,
-    Sorted,
-    Success,
-)
-from .sorting import (
-    OppSortBy,
-    OppSorting,
-    SortedResultsInfo,
-)
-from .types import (
-    DecimalString,
-    ISODate,
-    ISOTime,
-    UTCDateTime,
-)
+# Import all non-private names from each module
+from .base import *
+from .fields import *
+from .filters import *
+from .models import *
+from .pagination import *
+from .requests import *
+from .responses import *
+from .sorting import *
+from .types import *
 
-__all__ = [
-    # Base
-    "CommonGrantsBaseModel",
-    # Types
-    "DecimalString",
-    "ISODate",
-    "ISOTime",
-    "UTCDateTime",
-    # Fields
-    "CustomField",
-    "CustomFieldType",
-    "DateRangeEvent",
-    "Event",
-    "EventType",
-    "Money",
-    "OtherEvent",
-    "SingleDateEvent",
-    "SystemMetadata",
-    # Filters
-    "ArrayOperator",
-    "ComparisonOperator",
-    "DefaultFilter",
-    "DateComparisonFilter",
-    "DateRange",
-    "DateRangeFilter",
-    "EquivalenceOperator",
-    "InvalidMoneyValueError",
-    "MoneyComparisonFilter",
-    "MoneyRange",
-    "MoneyRangeFilter",
-    "OppDefaultFilters",
-    "OppFilters",
-    "RangeOperator",
-    "StringArrayFilter",
-    "StringComparisonFilter",
-    "StringOperator",
-    # Models
-    "OppFunding",
-    "OpportunityBase",
-    "OppStatus",
-    "OppStatusOptions",
-    "OppTimeline",
-    # Paginated
-    "PaginatedBase",
-    "PaginatedBodyParams",
-    "PaginatedQueryParams",
-    "PaginatedResultsInfo",
-    # Requests
-    "OpportunitySearchRequest",
-    # Responses
-    "DefaultResponse",
-    "Error",
-    "Filtered",
-    "FilterInfo",
-    "OpportunitiesListResponse",
-    "OpportunitiesSearchResponse",
-    "OpportunityResponse",
-    "Paginated",
-    "Sorted",
-    "Success",
-    # Sorting
-    "OppSortBy",
-    "OppSorting",
-    "SortedResultsInfo",
-]
+# Export all non-private names
+__all__ = [name for name in dir() if not name.startswith("_")]
