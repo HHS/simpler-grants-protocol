097b592fad2a8dbd7ce5029ce2f5e46e3df665b8
jcrichlake <jeff.crichlake@agile6.com>
2025-12-23 14:45:14 -0500

    Updating script and the validate.py object

--- lib/python-sdk/generate_models.sh
+++ lib/python-sdk/generate_models.sh
@@ -1,14 +1,13 @@
 echo "Processing all schemas"
 poetry run datamodel-codegen --input ../../website/public/schemas/yaml \
  --output common_grants_sdk/schemas/pydantic/generated \
+ --output-model-type pydantic_v2.BaseModel \
  --base-class common_grants_sdk.schemas.pydantic.base.CommonGrantsBaseModel \
- --reuse-model --reuse-scope tree \
+ --reuse-model \
  --field-constraints --use-standard-collections \
  --capitalise-enum-members \
  --use-specialized-enum \
  --target-python-version 3.13\
- --all-exports-scope recursive\
- --all-exports-collision-strategy minimal-prefix \
  --snake-case-field \
  --field-include-all-keys \
  --use-default-kwarg \
