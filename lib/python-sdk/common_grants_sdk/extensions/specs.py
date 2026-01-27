b0254ba03e908bf76306946e390ae39193303539
jcrichlake <jeff.crichlake@agile6.com>
2026-01-27 16:39:41 -0500

    Updating spec to not include pydantic fields as they were not being used

--- lib/python-sdk/common_grants_sdk/extensions/specs.py
+++ lib/python-sdk/common_grants_sdk/extensions/specs.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 from typing import Any, Optional
-from pydantic import Field
+
 from ..schemas.pydantic.fields.custom import CustomFieldType
 
 
@@ -10,8 +10,6 @@ from ..schemas.pydantic.fields.custom import CustomFieldType
 class CustomFieldSpec:
     """Custom Field spec class to support adding custom fields"""
 
-    key: str = Field(..., description="Custom field key name")
-    field_type: CustomFieldType = Field(
-        ..., alias="fieldType", description="Field type to be defined"
-    )
-    value: Optional[Any] = Field(default=None, description="schema definition value")
+    key: str 
+    field_type: CustomFieldType 
+    value: Optional[Any] = None
